9cac3bcefa355499d899b1b57f765a830fd9641d
luc45cubic <luc45cubic@gmail.com>
2022-06-29 19:44:28 +0200

    MessageSender

---
+++ MessageSender/MessageTemp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace MessageSender
+{
+    abstract public class MessageTemp
+    {
+        internal static string accountSid = "AC93ccc409edf104a7cdf128894a7d6ab2";
+        internal static string authToken = "e9e79c24d336051573abc1cc7f5a9086";
+
+        public static string PhoneNumber { get; set; }
+        public static string MessageBody { get; set; }
+
+        public MessageTemp(string phoneNumber, string messageBody)
+        {
+            PhoneNumber = phoneNumber;
+            MessageBody = messageBody;
+        }
+
+    }
+}
---
+++ MessageSender/SmsMessaging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace MessageSender
+{
+    public class SmsMessaging : MessageTemp
+    {
+        public SmsMessaging(string phoneNumber, string messageBody) : base(phoneNumber, messageBody)
+        {
+            PhoneNumber = phoneNumber;
+            MessageBody = messageBody;
+        }
+
+        public static void SendMessage()
+        {
+            
+
+            TwilioClient.Init(accountSid, authToken);
+            var messageOptions = new CreateMessageOptions(
+                new PhoneNumber(PhoneNumber));
+            messageOptions.MessagingServiceSid = "MG6b6cf34ea96b95f65627721f4baa0b6b";
+            messageOptions.Body = MessageBody;
+
+            MessageResource.Create(messageOptions);
+        }
+        
+    }
+}
