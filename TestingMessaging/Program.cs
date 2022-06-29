ac4849f87178a9b05df9e812f35f2540e3d29122
luc45cubic <luc45cubic@gmail.com>
2022-06-29 19:51:37 +0200

    TestingMessaging

---
+++ TestingMessaging/Program.cs
@@ -0,0 +1,10 @@
+﻿using MessageSender;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MessageTemp.PhoneNumber = "+420725925390";
+        MessageTemp.MessageBody = "Funguje to takhle"; 
+        SmsMessaging.SendMessage();
+    }
+}
