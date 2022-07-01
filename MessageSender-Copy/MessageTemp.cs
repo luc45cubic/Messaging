793688a237d9804d07b4359d611928a337ec0b67
luc45cubic <90869805+luc45cubic@users.noreply.github.com>
2022-07-01 15:47:39 +0200

    Add files via upload

---
+++ MessageSender-Copy/MessageTemp.cs
@@ -0,0 +1,29 @@
+﻿
+namespace MessageSender
+{
+    /// <summary>
+    /// Mezi vrstva pro všechny brány
+    /// </summary>
+    abstract public class MessageTemp
+    {
+        internal static string accountSid = "AC93ccc409edf104a7cdf128894a7d6ab2";
+        internal static string authToken = "e9e79c24d336051573abc1cc7f5a9086";
+
+        /// <summary>
+        /// Telefonní číslo na které se zpráva pošle
+        /// </summary>
+        public static string? PhoneNumber { get; set; }
+        /// <summary>
+        /// Samotná zpráva
+        /// </summary>
+        public static string? MessageBody { get; set; }
+
+        
+        public MessageTemp(string phoneNumber, string messageBody)
+        {
+            PhoneNumber = phoneNumber;
+            MessageBody = messageBody;
+        }
+
+    }
+}
